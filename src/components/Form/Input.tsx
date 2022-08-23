3b2fe4a682eb1aca01fe0fa882e4933dbc70a910
GuilhermeMota20 <guidroidbr.2007@gmail.com>
2022-08-23 13:47:02 -0300

    [FRONTEND] FT - Instalando react-hook-form e inicializando configuracoes

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,14 +1,15 @@
 import { FormControl, FormLabel, Input as ChakaraInput, InputProps as ChakraInputProps } from "@chakra-ui/react";
+import { forwardRef, ForwardRefRenderFunction } from "react";
 
 interface InputProps extends ChakraInputProps {
     name: string;
-    label?: string
+    label?: string;
 }
 
-export default function Input({ name, label, ...rest }: InputProps) {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({ name, label, ...rest }, ref) => {
     return (
         <FormControl>
-            { !!label && <FormLabel htmlFor={name}>{label}</FormLabel> }
+            {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
             <ChakaraInput
                 name={name}
                 id={name}
@@ -21,8 +22,11 @@ export default function Input({ name, label, ...rest }: InputProps) {
                     bgColor: 'gray.900'
                 }}
                 size='lg'
+                ref={ref}
                 {...rest}
             />
         </FormControl>
     )
 }
+
+export const Input = forwardRef(InputBase);
