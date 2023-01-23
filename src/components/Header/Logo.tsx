ed6bf4f9bd6ca9e661d7a1849ce842fdfc14de40
GuilhermeMota20 <guidroidbr.2007@gmail.com>
2023-01-23 16:18:56 -0300

    [FRONTEND] FT - Create new user

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -8,7 +8,7 @@ export function Logo() {
             letterSpacing='tight'
             w='64'
         >
-            DahsGoMota
+            DashGoMota
             <Text as='span' ml='1' color='pink.500'>.</Text>
         </Text>
     )
--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,11 +1,15 @@
 import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react";
 import { yupResolver } from '@hookform/resolvers/yup';
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { SubmitHandler, useForm } from 'react-hook-form';
+import { useMutation } from "react-query";
 import * as yup from 'yup';
 import { Input } from "../../components/Form/Input";
 import Header from "../../components/Header";
 import Sidebar from "../../components/Sidebar";
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
 
 type CreateUserFormData = {
     name: string;
@@ -24,17 +28,34 @@ const createUserFormSchema = yup.object().shape({
     ], 'As senhas precisam ser iguais'),
 });
 
-const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    console.log(values);
-}
-
 export default function CreateUser() {
+    const router = useRouter();
+
+    const createUser = useMutation(async (user: CreateUserFormData) => {
+        const response = await api.post('users', {
+            user: {
+                ...user,
+                created_at: new Date(),
+            }
+        })
+
+        return response.data.user;
+    }, {
+        onSuccess: ()=> {
+            queryClient.invalidateQueries('users');
+        }
+    });
+
     const { register, handleSubmit, formState } = useForm({
         resolver: yupResolver(createUserFormSchema)
     });
-
     const { errors } = formState;
+
+    const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
+        await createUser.mutateAsync(values);
+
+        router.push('/users');
+    };
 
     return (
         <Box>
